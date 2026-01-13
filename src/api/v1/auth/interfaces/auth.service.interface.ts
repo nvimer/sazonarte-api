9659c2452a07a96a4fcbb0844bfe6375acd2a2bc
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2026-01-12 19:29:42 -0500

    refactor(api): remove v1 directory for better manage of rest api

--- src/api/v1/auth/interfaces/auth.service.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@prisma/client";
-import { LoginInput } from "../auth.validator";
-
-/**
- * Auth Service Interface
- */
-export interface AuthServiceInterface {
-  login(data: LoginInput): Promise<User>;
-}
